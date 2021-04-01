114bbacd467574cb2c7e63039e5ff173fe17fe75
Daniil Matafonov <danielmt.dm@gmail.com>
2021-04-01 18:05:55 +0300

    subscriber test

---
+++ src/test/java/model/UserModel.java
@@ -0,0 +1,10 @@
+package model;
+
+public class UserModel {
+
+    public UserModel(String email) {
+        this.email = email;
+    }
+
+    private String email;
+}
---
+++ src/test/java/tests/DemoWebShopTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import com.github.javafaker.Faker;
+import com.google.gson.Gson;
+import model.UserModel;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.core.Is.is;
+
+public class DemoWebShopTest extends BaseTest {
+
+    final Faker faker = new Faker();
+    final Gson gson = new Gson();
+
+    @Test
+    public void subscribeToNews() {
+        final String emailAddress = faker.internet().emailAddress();
+        UserModel userModel = new UserModel(emailAddress);
+        final String userSubscribeJson = gson.toJson(userModel);
+        given().spec(requestSpecification)
+                .when().body(userSubscribeJson)
+                .post("/subscribenewsletter")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .log().body()
+                .body("Success",
+                        equalTo(Boolean.TRUE))
+                .body("Result", equalTo("Thank you for signing up! A verification email has been sent. We appreciate your interest."));
+    }
+}
